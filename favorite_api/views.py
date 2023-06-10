5e34a323f2fdd5fac193f23a014b4d4bc8a7722a
Dixon Frederick <dixon.frederick@ui.ac.id>
2023-06-10 23:33:58 +0700

    Add DELETE Favorites method

--- favorite_api/views.py
+++ favorite_api/views.py
@@ -5,6 +5,7 @@ from rest_framework import permissions
 from firebase_admin import firestore
 from rest_framework.permissions import IsAuthenticated
 from auth_api.authentication import FirebaseAuthentication
+import json
 
 class FavoriteListAPIView(APIView):
     # add permission to check if user is authenticated
@@ -20,4 +21,31 @@ class FavoriteListAPIView(APIView):
             "status": 200,
         }
         return Response(response_data)
+
+    def delete(self, request):
+        db = firestore.client()
+        favorite_ref = db.collection('favorites')
+        favorites = favorite_ref.get()
+        favorites_data = {}
+        body_unicode = request.body.decode('utf-8')
+        json_body = json.loads(body_unicode)
+
+        for favorite in favorites:
+            favorite_id = favorite.id
+            favorite_data = favorite.to_dict()
+            favorites_data[favorite_id] = favorite_data
+            if favorite_data['cafe_id'] == json_body['cafe_id'] and favorite_data['user_id'] == json_body['user_id']:
+                deleted_ref = db.collection('favorites').document(favorite_id)
+                response_data = {
+                    "status": 200,
+                }
+                deleted_ref.delete()
+                return Response(response_data)
+
+        response_data = {
+                    "message": "not found",
+                }
+        return Response(response_data)
+        
+        
 
