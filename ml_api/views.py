28e9f977de91536943dd2a809d7b210a5728f36e
hamonanganN <bornyto.hamonangan@ui.ac.id>
2023-06-12 08:43:53 +0700

    enable communication with ml api

--- ml_api/views.py
+++ ml_api/views.py
@@ -2,8 +2,6 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import permissions
-from .models import Cafe
-from .serializers import CafeSerializer
 from firebase_admin import firestore
 from rest_framework.permissions import IsAuthenticated
 from auth_api.authentication import FirebaseAuthentication
@@ -18,38 +16,39 @@ class RecommendationAPIView(APIView):
     def get(self, request):
         current_username = request.user.username
         db = firestore.client()
-        user = db.collection('users').document(current_username)
+        user = db.collection('users').document(current_username).get().to_dict()
         
         ML_API_URL = os.getenv("ML_API_URL")
         
         payload = {
             "new_user_id": current_username, 
-            "new_is_male": True, # placeholder
+            "new_is_male": 1, # placeholder
             "new_age_group": 1, # placeholder
-            "new_price_category": user.cafe_price_category,
-            "new_24hrs": True,
-            "new_outdoor": user.cafe_outdoor,
-            "new_smoking_area": user.cafe_smoking_area,
-            "new_parking_area": user.cafe_parking_area,
-            "new_pet_friendly": user.cafe_pet_friendly,
-            "new_wifi": user.cafe_wifi,
-            "new_indoor": user.cafe_indoor,
-            "new_live_music": user.cafe_live_music,
-            "new_takeaway": user.cafe_takeaway,
-            "new_kid_friendly": user.cafe_kid_friendly,
-            "new_alcohol": user.cafe_alcohol,
-            "new_in_mall": user.cafe_in_mall,
-            "new_toilets": user.cafe_toilets,
-            "new_reservation": user.cafe_reservations,
-            "new_vip_room": user.cafe_vip_room
+            "new_price_category": 1 if user["cafe_price_category"]=="$" else (2 if user["cafe_price_category"]=="$$" else 3),
+            "new_24hrs": 1, # placeholder
+            "new_outdoor": 1 if user["cafe_outdoor"] else 0,
+            "new_smoking_area": 1 if user["cafe_smoking_area"] else 0,
+            "new_parking_area": 1 if user["cafe_parking_area"] else 0,
+            "new_pet_friendly": 1 if user["cafe_pet_friendly"] else 0,
+            "new_wifi": 1 if user["cafe_wifi"] else 0,
+            "new_indoor": 1 if user["cafe_indoor"] else 0,
+            "new_live_music": 1 if user["cafe_live_music"] else 0,
+            "new_takeaway": 1 if user["cafe_takeaway"] else 0,
+            "new_kid_friendly": 1 if user["cafe_kid_friendly"] else 0,
+            "new_alcohol": 1 if user["cafe_alcohol"] else 0,
+            "new_in_mall": 1 if user["cafe_in_mall"] else 0,
+            "new_toilets": 1 if user["cafe_toilets"] else 0,
+            "new_reservation": 1 if user["cafe_reservation"] else 0,
+            "new_vip_room": 1 if user["cafe_vip_room"] else 0
         }
 
         res = requests.post(ML_API_URL, json=payload)
 
+        print(res.text)
         response_data = {
             "status": 200,
-            "data": res.json
+            "data": res.json()
         }
-        
+
         return Response(response_data, status=status.HTTP_200_OK)
         
