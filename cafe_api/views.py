39405a142a4f27fe377181261c01e80255e96464
Dixon Frederick <dixon.frederick@ui.ac.id>
2023-06-14 02:23:38 +0700

    Implement Search, Filter, Ordering

--- cafe_api/views.py
+++ cafe_api/views.py
@@ -17,6 +17,39 @@ class CafeListAPIView(APIView):
         if cafe_id is None:
             db = firestore.client()
             cafes_ref = db.collection('cafes')
+
+            # Search
+            search_query = request.GET.get('search', '')
+            if search_query:
+                search_query = search_query.lower()
+                cafes = cafes_ref.get()
+                cafes_data = []
+        
+                for cafe in cafes:
+                    cafe_data = cafe.to_dict()
+                    if cafe_data['name'].lower().find(search_query) > -1 :
+                        cafes_data.append(cafe_data)
+                response_data = {
+                    "status": 200,
+                    "data": cafes_data
+                }
+                return Response(response_data, status=status.HTTP_200_OK)
+    
+            # Filtering
+            filter_params = request.GET.getlist('filter')
+            for param in filter_params:
+                cafes_ref = cafes_ref.where(param, '==', True)
+            min_rating = request.GET.get('min_rating', '')
+            if min_rating:
+                min_rating = float(min_rating)
+                cafes_ref = cafes_ref.where('rating', '>=', min_rating)
+                
+            # Ordering
+            order_query = request.GET.get('order', '')
+            reverse_order = request.GET.get('reverse', '') == 'true'
+            if order_query:
+                cafes_ref = cafes_ref.order_by(order_query, direction=firestore.Query.DESCENDING if reverse_order else firestore.Query.ASCENDING)
+
             cafes = cafes_ref.get()
             cafes_data = []
             for cafe in cafes:
